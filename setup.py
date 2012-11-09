a885bd717fd20047585e849cca5e8b12bf581f3f
Andreas kaiser <disko@binary-punks.com>
2012-11-09 15:10:08 +0100

    Code cosmetics.

--- setup.py
+++ setup.py
@@ -3,46 +3,49 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.2'
+version = '0.3'
 project = 'kotti_theme_cyborg'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-setup(name=project,
-      version=version,
-      description="Cyborg theme from http://bootswatch.com/cyborg/ for Kotti",
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+setup(
+    name=project,
+    version=version,
+    description="Cyborg theme from http://bootswatch.com/cyborg/ for Kotti",
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "License :: BSD",
-        ],
-      keywords='kotti theme',
-      author='Andreas Kaiser',
-      author_email='disko@binary-punks.com',
-      url='https://github.com/disko/kotti_theme_cyborg',
-      license='bsd',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'Kotti',
-      ],
-      entry_points={
+    ],
+    keywords='kotti theme',
+    author='Andreas Kaiser',
+    author_email='disko@binary-punks.com',
+    url='https://github.com/disko/kotti_theme_cyborg',
+    license='bsd',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Kotti',
+    ],
+    entry_points={
         'fanstatic.libraries': [
-          'kotti_theme_cyborg = kotti_theme_cyborg.static:library',
+            'kotti_theme_cyborg = kotti_theme_cyborg.static:library',
         ],
-      },
-      extras_require={
+    },
+    extras_require={
         'development': [
             'minify',
         ]
-      },
-      message_extractors={'kotti_theme_cyborg': [
+    },
+    message_extractors={
+        'kotti_theme_cyborg': [
             ('**.py', 'lingua_python', None),
             ('**.zcml', 'lingua_xml', None),
             ('**.pt', 'lingua_xml', None),
-            ]},
-      )
+        ]
+    },
+)
